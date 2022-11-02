8a3a1d6e278286a352168db3d3624dd6431acc17
gvalderrey <gvalderrey@outlook.com>
2022-11-02 12:09:48 +0100

    agrego funcion2

--- powershell_task1.ps1
+++ powershell_task1.ps1
@@ -4,7 +4,15 @@ function activity1($act_param1){
     return $result
 }
 
+function activity2($act_param2){ 
+    $act_param1 
+    $result = $act_param2 + " algo mas"
+    return $result
+}
+
 $var = "Gustavo"
-activity1 -act_param1 $var
+$act1_result = activity1 -act_param1 $var
+
+activity2 -act_param2 $act1_result
 
 
